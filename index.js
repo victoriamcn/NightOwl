2b646e0e961341be99e8083197d5e2b67b501cd0
Anabel-Espinoza <anabel.espinoza@gmail.com>
2023-03-05 13:38:31 -0500

    Added date picker. JS calls for TicketMaster info on selected date

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ let isMusic = document.querySelector('#music')
 let isSports = document.querySelector('#sports')
 let isArts = document.querySelector('#arts')
 let searchBtn = document.querySelector('#searchBtn')
+let inputDate = document.querySelector('#inputDate')
 let sports = ""
 let arts= ""
 let music= ""
 
 // GET TicketMaster Events
 function getTmEvents() {
-    if (!city.value) {
-        console.log ('Enter city name')
+    if (!city.value || !inputDate.value) {
+        console.log ('Enter city name and date')
         } else {
         if (!isSports.checked && !isMusic.checked && !isArts.checked){
             console.log('Need to add at least one type of event')
         } else {
+        console.log(inputDate.value, )
         getActivityType()
-        let apiTM = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US'+ sports + arts + music  + '&city=' + city.value + '&startDateTime=2023-05-01T17:00:00Z' + '&endDateTime=2023-05-02T17:00:00Z' + '&apikey=' + tmKey
+        let dayBefore = dayjs(inputDate.value).subtract(1, 'day').format('YYYY-MM-DD')
+        let dayAfter = dayjs(inputDate.value).add(1, 'day').format('YYYY-MM-DD')
+        console.log(dayBefore, dayAfter)
+        let apiTM = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US'+ sports + arts + music  + '&city=' + city.value + '&radius=100&unit=miles' + '&startDateTime='+ dayBefore + 'T23:59:59Z&endDateTime=' + dayAfter + 'T00:00:00Z&apikey=' + tmKey
         fetch(apiTM)
             .then(function(response) {
                 if (!response.ok) {
