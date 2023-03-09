6ffff6e9e32f71eb7225fbad5d9611883079f6b5
Victoria <vmcnorrill@gmail.com>
2023-03-08 19:16:51 -0500

    fixed typos

---
+++ src/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    './index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
